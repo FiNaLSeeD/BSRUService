db0e7b7dbf05b8b05ee6e7a9b25512a7f9eba8bf
guideza <guide_zana@hotmail.co.th>
2019-02-26 16:08:26 +0700

    Finish 26/2/2019

--- app/src/main/java/lalit/bsru/ac/th/bsruservice/MainFragment.java
+++ app/src/main/java/lalit/bsru/ac/th/bsruservice/MainFragment.java
@@ -1,11 +1,14 @@
 package lalit.bsru.ac.th.bsruservice;
 
 
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.support.annotation.Nullable;
+        import android.support.v4.app.Fragment;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+        import android.widget.TextView;
 
 
 /**
@@ -18,6 +21,31 @@ public class MainFragment extends Fragment {
         // Required empty public constructor
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        // Register Controller
+        registerController();
+
+
+    }    //Main Method
+
+    private void registerController() {
+        TextView textView = getView().findViewById(R.id.txtRegister);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//          Replace Fragment
+                getActivity()
+                        .getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.layoutMainFragment, new RegisterFragment())
+                        .commit();
+
+            }
+        });
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
