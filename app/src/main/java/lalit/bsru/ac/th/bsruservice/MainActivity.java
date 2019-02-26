d1419c0c892354cf0d375f6d3565ae9dc6818a89
guideza <guide_zana@hotmail.co.th>
2019-02-26 12:12:13 +0700

    Initial commit

---
+++ app/src/main/java/lalit/bsru/ac/th/bsruservice/MainActivity.java
@@ -0,0 +1,21 @@
+package lalit.bsru.ac.th.bsruservice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//  Add Fragment to Activity
+                getSupportFragmentManager().beginTransaction().add(R.id.layoutMainFragment, new MainFragment()).commit();
+
+
+
+
+    }    //Main method
+
+}     //Main Class
